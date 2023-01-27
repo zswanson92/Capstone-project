5ede6f6bc1d76976769ff378d27852159f0099a6
zswanson92 <zackta92@gmail.com>
2023-01-26 22:42:17 -0600

    added validations for menu and menu item name lengths as well as specific image url uploads

--- react-app/src/components/CreateMenu/CreateMenu.js
+++ react-app/src/components/CreateMenu/CreateMenu.js
@@ -18,7 +18,7 @@ const CreateMenu = () => {
 
     const onSubmit = async (e) => {
         e.preventDefault()
-        if(errors.length > 0) return;
+        if (errors.length > 0) return;
         const createdMenu = {
             userId,
             businessId,
@@ -29,13 +29,37 @@ const CreateMenu = () => {
         const theNewMenu = await dispatch(createMenuThunk(createdMenu))
 
         // history.push(`/businesses/${businessId}`)
-        if(theNewMenu){
+        if (theNewMenu) {
             await history.push(`/create/menu/${theNewMenu.id}`)
         }
     }
 
     const onClose = () => {
         history.push(`/businesses/${businessId}`);
+    }
+
+    useEffect(() => {
+
+        let err = [];
+
+        if (menu_image.length > 0 && !validImageUrl(menu_image)) {
+            errors.push("Please submit as jpg, jpeg, or png.")
+        }
+
+        setErrors(err)
+    }, [menu_image])
+
+    function validImageUrl(url) {
+        let falseycheck;
+        let lastThree = url.split('').slice(url.length - 3)
+        // console.log(lastThree.join(''))
+        if (lastThree.join('') === 'png' || lastThree.join('') === 'jpg' || lastThree.join('') === 'peg') {
+            falseycheck = true
+        } else {
+            falseycheck = false
+        }
+        // console.log(falseycheck)
+        return falseycheck
     }
 
 
@@ -45,26 +69,28 @@ const CreateMenu = () => {
                 <h1>Create a Menu</h1>
                 <div className="menu-form-divs">
                     <input
-                    required={true}
-                    type='text'
-                    onChange={(e) => setCategory(e.target.value)}
-                    value={category}
-                    placeholder="Name of Menu Category"
-                    className="menu-category-input"
+                        required={true}
+                        type='text'
+                        onChange={(e) => setCategory(e.target.value)}
+                        value={category}
+                        placeholder="Name of Menu Category"
+                        className="menu-category-input"
                     />
+                    {category.length > 40 ? <div>Name of menu must be less than 40 characters.</div> : ""}
                 </div>
                 <div className="menu-form-divs">
                     <input
-                    type='text'
-                    onChange={(e) => setMenuImage(e.target.value)}
-                    value={menu_image}
-                    placeholder="Optional URL image for Menu Category"
-                    className="menu-category-input"
+                        type='text'
+                        onChange={(e) => setMenuImage(e.target.value)}
+                        value={menu_image}
+                        placeholder="Optional URL image for Menu Category"
+                        className="menu-category-input"
                     />
+                    {menu_image.length > 0 && !validImageUrl(menu_image) ? <div>If submitting an image, it must be jpg, jpeg, or png format.</div> : ""}
                 </div>
                 <div className="create-menu-two-buttons-div">
-                {errors.length ? "" : <button type='submit' className="create-menu-return-button">Submit Menu</button>}
-                <button className="create-menu-return-button" onClick={onClose}>Return to Business</button>
+                    {errors.length ? "" : <button type='submit' className="create-menu-return-button">Submit Menu</button>}
+                    <button className="create-menu-return-button" onClick={onClose}>Return to Business</button>
                 </div>
             </form>
         </div>
